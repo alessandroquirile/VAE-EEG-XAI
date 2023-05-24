fd58bb83f8c4b3c6158406989343714bafb9cbd8
alessandroquirile <a.quirile@studenti.unina.it>
2023-05-24 12:58:00 +0200

    Added further explanations of intent

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from utils import *
 if __name__ == '__main__':
     path = 'data_original/'
     sample_rate = 128
-    l_freq = 1  # Significant brain activities are usually between 1 Hz...
-    h_freq = 10  # ...and 10 Hz
+    l_freq = 1
+    h_freq = 10
 
     subjects = get_subjects(path)
     for subject in subjects:
@@ -20,9 +20,10 @@ if __name__ == '__main__':
 
         indices = get_indices_where_video_start(raw)
         for trial, index in enumerate(indices, start=1):
-            cropped_raw = crop(raw, index),
+            # Crop the raw signal based on the 60-seconds-trial-length at given index
+            cropped_raw = crop(raw, index)
             save_plot('plots', l_freq, h_freq, cropped_raw, subject, trial)
-
             thresh = calculate_threshold(lowest_peak, highest_peak)
+            # We shall focus on EEG since Fp1, Fp2 are the closest to the eyes
             events = find_eog_events(cropped_raw, ch_name=EEG, thresh=thresh)
             save_events('events', subject, trial, events, index, sample_rate)
